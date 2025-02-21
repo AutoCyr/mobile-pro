4ae925aa34be6d2770967de2788a009448330986
Ginott Log <ginlogossou@gmail.com>
2025-02-21 13:18:55 +0000

    Partner pieces updates #2

--- lib/presentation/ui/organisms/overviews/large_overview.dart
+++ lib/presentation/ui/organisms/overviews/large_overview.dart
@@ -4,50 +4,60 @@ import 'package:autocyr_pro/presentation/ui/core/theme.dart';
 import 'package:flutter/material.dart';
 
 Widget LargeOverview({
+  required BuildContext context,
   required String label,
   required String value,
   required IconData icon,
   required Size size,
+  Widget? child,
 }) {
-  return Container(
-    width: size.width,
-    height: 100,
-    decoration: BoxDecoration(
-      color: GlobalThemeData.lightColorScheme.onPrimary,
-      border: Border.all(
-          color: GlobalThemeData.lightColorScheme.primaryContainer.withOpacity(0.5),
-          width: 1
+  return InkWell(
+    splashColor: Colors.transparent,
+    onTap: () {
+      if(child != null) {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => child));
+      }
+    },
+    child: Container(
+      width: size.width,
+      height: 100,
+      decoration: BoxDecoration(
+        color: GlobalThemeData.lightColorScheme.onPrimary,
+        border: Border.all(
+            color: GlobalThemeData.lightColorScheme.primaryContainer.withOpacity(0.5),
+            width: 1
+        ),
       ),
-    ),
-    child: Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Label12(
-                    text: label,
-                    color: GlobalThemeData.lightColorScheme.secondaryContainer,
-                    weight: FontWeight.bold,
-                    maxLines: 1
-                ),
-                Label30(
-                    text: value,
-                    color: GlobalThemeData.lightColorScheme.primaryContainer,
-                    weight: FontWeight.bold,
-                    maxLines: 1
-                ),
-              ]
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Label12(
+                      text: label,
+                      color: GlobalThemeData.lightColorScheme.secondaryContainer,
+                      weight: FontWeight.bold,
+                      maxLines: 1
+                  ),
+                  Label30(
+                      text: value,
+                      color: GlobalThemeData.lightColorScheme.primaryContainer,
+                      weight: FontWeight.bold,
+                      maxLines: 1
+                  ),
+                ]
+            ),
           ),
-        ),
-        Positioned(
-          bottom: -70,
-          right: -30,
-          child: Icon(icon, color: GlobalThemeData.lightColorScheme.primary.withOpacity(0.5), size: size.width * 0.35,),
-        ),
-      ],
+          Positioned(
+            bottom: -70,
+            right: -30,
+            child: Icon(icon, color: GlobalThemeData.lightColorScheme.primary.withOpacity(0.5), size: size.width * 0.35,),
+          ),
+        ],
+      ),
     ),
   );
 }
--- lib/presentation/ui/screens/masters/home.dart
+++ lib/presentation/ui/screens/masters/home.dart
@@ -1,4 +1,5 @@
 import 'package:autocyr_pro/presentation/notifier/auth_notifier.dart';
+import 'package:autocyr_pro/presentation/notifier/partner_notifier.dart';
 import 'package:autocyr_pro/presentation/ui/atoms/labels/label10.dart';
 import 'package:autocyr_pro/presentation/ui/atoms/labels/label12.dart';
 import 'package:autocyr_pro/presentation/ui/atoms/labels/label13.dart';
@@ -60,11 +61,20 @@ class _HomeDashScreenState extends State<HomeDashScreen> {
     await auth.updateFCM(context: context);
   }
 
+  loadDatas() async {
+    final auth = Provider.of<AuthNotifier>(context, listen: false);
+    final partner = Provider.of<PartnerNotifier>(context, listen: false);
+    if(partner.pieces.isEmpty) {
+      await partner.getPieces(id: auth.getUser.id.toString(), context: context);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       updateFCM();
+      loadDatas();
     });
   }
 
@@ -72,212 +82,169 @@ class _HomeDashScreenState extends State<HomeDashScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Consumer2<AuthNotifier, PartnerNotifier>(
+        builder: (context, auth, partner, child) {
+
+          return SafeArea(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
               children: [
-                InkWell(
-                  onTap: () => BottomSelector().showIconMenu(
-                    context: context,
-                    options: options,
-                    title: "Options"
-                  ),
-                  splashColor: Colors.transparent,
-                  child: Icon(
-                    Icons.menu_rounded,
-                    color: GlobalThemeData.lightColorScheme.primaryContainer,
-                  ),
-                ),
-                Image.asset(
-                  "assets/logos/auto.png",
-                  width: 50,
-                  height: 50,
-                )
-                /*Icon(
-                  Icons.notifications_on_rounded,
-                  color: GlobalThemeData.lightColorScheme.primaryContainer,
-                ),*/
-              ]
-            ).animate().fadeIn(),
-            const Gap(30),
-            Label20(
-              text: "Tableau de bord",
-              color: GlobalThemeData.lightColorScheme.secondaryContainer,
-              weight: FontWeight.bold,
-              maxLines: 1
-            ).animate().fadeIn(),
-            const Gap(10),
-            Label14(
-              text: "Maison LOG Pieces",
-              color: GlobalThemeData.lightColorScheme.secondary,
-              weight: FontWeight.normal,
-              maxLines: 2
-            ).animate().fadeIn(),
-            const Gap(20),
-            /*if(visible)
-            Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: GlobalThemeData.lightColorScheme.errorContainer.withOpacity(0.6),
-                    border: Border.all(
-                      color: GlobalThemeData.lightColorScheme.onErrorContainer,
-                    )
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Label14(
-                            text: "Abonnement bientôt expiré",
-                            color: GlobalThemeData.lightColorScheme.onErrorContainer,
-                            weight: FontWeight.bold,
-                            maxLines: 1
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              updateVisible();
-                            },
-                            child: Icon(
-                              Icons.clear,
-                              color: GlobalThemeData.lightColorScheme.onErrorContainer,
-                              size: 24,
-                            ),
-                          )
-                        ],
-                      ),
-                      const Gap(10),
-                      Label12(
-                        text: "Renouvelez votre abonnement pour continuer à profiter de nos services. \nMerci pour votre fidélité.",
-                        color: GlobalThemeData.lightColorScheme.onErrorContainer,
-                        weight: FontWeight.bold,
-                        maxLines: 2
-                      ),
-                    ],
-                  ),
-                ).animate().fadeIn(),
-                const Gap(20),
-              ],
-            ),*/
-            Column(
-              children: [
-                Container(
-                  width: size.width,
-                  height: 150,
-                  decoration: BoxDecoration(
-                      color: GlobalThemeData.lightColorScheme.surfaceTint.withOpacity(0.7),
-                      border: Border.all(
-                        color: GlobalThemeData.lightColorScheme.surfaceTint,
-                      )
-                  ),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        right: -120,
-                        top: -40,
-                        child: Image.asset(
-                          "assets/pngs/map.png",
-                          width: size.width * 0.8,
-                          height: size.width * 0.8,
+                      InkWell(
+                        onTap: () => BottomSelector().showIconMenu(
+                            context: context,
+                            options: options,
+                            title: "Options"
+                        ),
+                        splashColor: Colors.transparent,
+                        child: Icon(
+                          Icons.menu_rounded,
+                          color: GlobalThemeData.lightColorScheme.primaryContainer,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: size.width * 0.7,
-                              child: Label13(
-                                  text: "Faites connaitre la position de vos boutiques",
-                                  color: GlobalThemeData.lightColorScheme.onErrorContainer,
-                                  weight: FontWeight.bold,
-                                  maxLines: 2
-                              ),
+                      Image.asset(
+                        "assets/logos/auto.png",
+                        width: 50,
+                        height: 50,
+                      )
+                    ]
+                ).animate().fadeIn(),
+                const Gap(30),
+                Label20(
+                    text: "Tableau de bord",
+                    color: GlobalThemeData.lightColorScheme.secondaryContainer,
+                    weight: FontWeight.bold,
+                    maxLines: 1
+                ).animate().fadeIn(),
+                const Gap(10),
+                Label14(
+                    text: auth.getPartenaire.raisonSociale,
+                    color: GlobalThemeData.lightColorScheme.secondary,
+                    weight: FontWeight.normal,
+                    maxLines: 2
+                ).animate().fadeIn(),
+                const Gap(20),
+                Column(
+                  children: [
+                    Container(
+                      width: size.width,
+                      height: 150,
+                      decoration: BoxDecoration(
+                          color: GlobalThemeData.lightColorScheme.surfaceTint.withOpacity(0.7),
+                          border: Border.all(
+                            color: GlobalThemeData.lightColorScheme.surfaceTint,
+                          )
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            right: -120,
+                            top: -40,
+                            child: Image.asset(
+                              "assets/pngs/map.png",
+                              width: size.width * 0.8,
+                              height: size.width * 0.8,
                             ),
-                            const Gap(10),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 SizedBox(
-                                  width: size.width * 0.6,
-                                  child: Label12(
-                                      text: "Augmentez votre visibilité en permettant aux clients de facilement vous retrouver pour booster votre chiffre d'affaire.",
+                                  width: size.width * 0.7,
+                                  child: Label13(
+                                      text: "Faites connaitre la position de vos boutiques",
                                       color: GlobalThemeData.lightColorScheme.onErrorContainer,
-                                      weight: FontWeight.normal,
-                                      maxLines: 3
+                                      weight: FontWeight.bold,
+                                      maxLines: 2
                                   ),
                                 ),
-                                CustomIconButton(
-                                    icon: Icons.arrow_forward_ios_rounded,
-                                    size: size,
-                                    context: context,
-                                    function: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const AddressListScreen())),
-                                    iconColor: GlobalThemeData.lightColorScheme.primary,
-                                    buttonColor: GlobalThemeData.lightColorScheme.onPrimary,
-                                    backColor: GlobalThemeData.lightColorScheme.primary
+                                const Gap(10),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    SizedBox(
+                                      width: size.width * 0.6,
+                                      child: Label12(
+                                          text: "Augmentez votre visibilité en permettant aux clients de facilement vous retrouver pour booster votre chiffre d'affaire.",
+                                          color: GlobalThemeData.lightColorScheme.onErrorContainer,
+                                          weight: FontWeight.normal,
+                                          maxLines: 3
+                                      ),
+                                    ),
+                                    CustomIconButton(
+                                        icon: Icons.arrow_forward_ios_rounded,
+                                        size: size,
+                                        context: context,
+                                        function: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const AddressListScreen())),
+                                        iconColor: GlobalThemeData.lightColorScheme.primary,
+                                        buttonColor: GlobalThemeData.lightColorScheme.onPrimary,
+                                        backColor: GlobalThemeData.lightColorScheme.primary
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ).animate().fadeIn(),
+                    const Gap(20),
+                  ],
+                ),
+                LargeOverview(
+                  context: context,
+                  label: "Total pièces enregistrées",
+                  value: partner.mainLoading && partner.pieces.isEmpty ? "..." : partner.pieces.length.toString(),
+                  icon: Icons.settings_rounded,
+                  size: size,
+                  child: const PieceListScreen()
                 ).animate().fadeIn(),
-                const Gap(20),
+                const Gap(5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SmallOverview(
+                      context: context,
+                      label: "Total commandes motos",
+                      value: "07",
+                      icon: Icons.motorcycle_rounded,
+                      size: size
+                    ).animate().fadeIn(),
+                    SmallOverview(
+                      context: context,
+                      label: "Total commandes auto",
+                      value: "40",
+                      icon: Icons.car_crash_rounded,
+                      size: size
+                    ).animate().fadeIn()
+                  ],
+                ),
+                const Gap(5),
+                LargeOverview(
+                  context: context,
+                  label: "Total interventions",
+                  value: "15",
+                  icon: Icons.scuba_diving_rounded,
+                  size: size
+                ).animate().fadeIn(),
+                const Gap(5),
+                LargeOverview(
+                  context: context,
+                  label: "Total contacts établis",
+                  value: "75",
+                  icon: Icons.webhook_rounded,
+                  size: size
+                ).animate().fadeIn(),
               ],
             ),
-            LargeOverview(
-              label: "Total pièces enregistrées",
-              value: "17",
-              icon: Icons.settings_rounded,
-              size: size
-            ).animate().fadeIn(),
-            const Gap(5),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SmallOverview(
-                    label: "Total commandes motos",
-                    value: "07",
-                    icon: Icons.motorcycle_rounded,
-                    size: size
-                ).animate().fadeIn(),
-                SmallOverview(
-                    label: "Total commandes auto",
-                    value: "40",
-                    icon: Icons.car_crash_rounded,
-                    size: size
-                ).animate().fadeIn()
-              ],
-            ),
-            const Gap(5),
-            LargeOverview(
-                label: "Total interventions",
-                value: "15",
-                icon: Icons.scuba_diving_rounded,
-                size: size
-            ).animate().fadeIn(),
-            const Gap(5),
-            LargeOverview(
-                label: "Total contacts établis",
-                value: "75",
-                icon: Icons.webhook_rounded,
-                size: size
-            ).animate().fadeIn(),
-          ],
-        ),
-      ),
+          );
+        }
+      )
     );
   }
 }
